dc83053cd017af81961f8f98d389ce1301e779f9
hjgel <202244111@itc.ac.kr>
2023-10-23 17:43:42 +0900

    week09 v1.9

--- go/week09/main.go
+++ go/week09/main.go
@@ -2,15 +2,33 @@ package main
 
 import "fmt"
 
-func double(n *int) {
-	*n = *n * 2
+func swap(n1 *int, n2 *int) {
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+}
+func main() {
+	a := 10
+	b := 20
+
+	c := &a
+
+	fmt.Printf("%d %d %d\n", a, b, *c)
 }
 
-func main() {
-	var a int = 5
-	double(&a) // pass by pointer
-	fmt.Printf("%d\n", a)
-}
+// package main
+
+// import "fmt"
+
+// func double(n *int) {
+// 	*n = *n * 2
+// }
+
+// func main() {
+// 	var a int = 5
+// 	double(&a) // pass by pointer
+// 	fmt.Printf("%d\n", a)
+// }
 
 // package main
 
