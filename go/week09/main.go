63db5269b83f7c9f255a63b550f82815f3c11d0f
hjgel <202244111@itc.ac.kr>
2023-10-23 17:35:28 +0900

    week09 v1.7

--- go/week09/main.go
+++ go/week09/main.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func double(n int) {
-	n = n * 2
+func double(n *int) {
+	*n = *n * 2
 }
 
 func main() {
 	var a int = 5
-	double(a)
+	double(&a) // pass by pointer
 	fmt.Printf("%d\n", a)
 }
 
