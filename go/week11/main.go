2d22aa9ede192e6c02594cf374a79d5420001e0e
hjgel <202244111@itc.ac.kr>
2023-11-06 17:37:50 +0900

    v2.5

--- go/week11/main.go
+++ go/week11/main.go
@@ -21,10 +21,27 @@ func main() {
 
 	// fmt.Println(primes[3]) // 컴파일 에러
 
-	i := 0
-	for i < 3 { // 4로하면? panic: runtime error: index out of range [3] with length 3
-		fmt.Println((primes[i]))
-		i++
+	// i := 0
+	// for i < 3 { // 4로하면? panic: runtime error: index out of range [3] with length 3
+	// 	fmt.Println((primes[i]))
+	// 	i++
+	// }
+
+	// for i := 0; i < len(primes); i++ {
+	// 	fmt.Println(primes[i])
+	// }
+
+	// for idx, prime := range primes {
+	// 	fmt.Println(idx, prime)
+	// }
+	// // enumirate()와 같이 사용할 수 있는 range
+
+	// for idx, prime := range primes { // 선언하고 변수를 사용 안 함.
+	// 	fmt.Println(prime)
+	// }
+
+	for _, prime := range primes { // 인덱스 사용 안 한다는 뜻
+		fmt.Println(prime)
 	}
 
 }
