83619e985fedbeecce7bcee04ce41f0635d9d23b
hjgel <202244111@itc.ac.kr>
2023-11-06 17:15:39 +0900

    v2.4

--- go/week11/main.go
+++ go/week11/main.go
@@ -8,7 +8,23 @@ func main() {
 	// primes[1] = 3
 	// primes[2] = 5
 
-	// var primes [3]int = [3]int{2, 3, 5}
+	// var primes [3]int = [3]int{2, 3, 5} //배열 리터럴로 primes 배열 초기화
 	primes := [3]int{2, 3, 5}
 	fmt.Println(primes, primes[1])
+
+	test := [5]bool{true, true, true}
+	fmt.Println(test[3])
+
+	// for i := 0; i < 3; i++ {
+	// fmt.Println((primes[i]))
+	// }
+
+	// fmt.Println(primes[3]) // 컴파일 에러
+
+	i := 0
+	for i < 3 { // 4로하면? panic: runtime error: index out of range [3] with length 3
+		fmt.Println((primes[i]))
+		i++
+	}
+
 }
