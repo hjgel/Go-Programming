95fc92d9ae9a7cb246ca2c1dddb88a8e16371340
hjgel <202244111@itc.ac.kr>
2023-11-13 17:43:04 +0900

    week12 v3.2

--- go/week12/main.go
+++ go/week12/main.go
@@ -3,14 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	a := []string{"a", "b", "c", "d"}
-	as := a[:2]
-	a[1] = "Z"
-	fmt.Println(a, as)
 
-	b := [4]int{4, 3, 2, 1}
-	bs := b[1:3]
-	fmt.Println(b, bs)
+	a := make([]string, 4, 5) // type, length, capacity
+	a[0] = "a"
+	a[2] = "b"
+	a[3] = "c"
+	as := a[0:2]
+	as[1] = "Z"
+	c := append(a, "y")
+
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(c, len(c), cap(c))
+
+	// a := []string{"a", "b", "c", "d"}
+	// as := a[:2]
+	// a[1] = "Z"
+	// fmt.Println(a, as)
+
+	// b := [4]int{4, 3, 2, 1}
+	// bs := b[1:3]
+	// fmt.Println(b, bs)
+
 	// // var s []int
 	// // s = make([]int, 5)
 
