f6c0cca008e3d922437fe0484ac24b103ecf4982
hjgel <202244111@itc.ac.kr>
2023-11-13 17:50:50 +0900

    week12 v3.3

--- go/week12/main.go
+++ go/week12/main.go
@@ -10,10 +10,12 @@ func main() {
 	a[3] = "c"
 	as := a[0:2]
 	as[1] = "Z"
-	c := append(a, "y")
+	c := append(a, "y", "x")
 
+	c[0] = "q"
 	fmt.Println(a, len(a), cap(a))
 	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
 
 	// a := []string{"a", "b", "c", "d"}
 	// as := a[:2]
