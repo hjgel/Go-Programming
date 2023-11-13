b690e6f8a4c8589940f888bf28f097debef5cc38
hjgel <202244111@itc.ac.kr>
2023-11-13 17:31:38 +0900

    week12 v3.1

--- go/week12/main.go
+++ go/week12/main.go
@@ -3,34 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	// var s []int
-	// s = make([]int, 5)
+	a := []string{"a", "b", "c", "d"}
+	as := a[:2]
+	a[1] = "Z"
+	fmt.Println(a, as)
 
-	// s := make([]int, 5) // 단축연산자 및 슬라이스 리터럴 사용, 변수 선언과 동시에 메모리 할당, 원소들은 제로값으로 초기화.
-	s := []int{0, 0, 0, 0, 0} // 변수 선언과 동시에 메모리 할당, 원소 초기화
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(b, bs)
+	// // var s []int
+	// // s = make([]int, 5)
 
-	s[4] = 100
-	s[0] = 7
-	s[1] = 91
+	// // s := make([]int, 5) // 단축연산자 및 슬라이스 리터럴 사용, 변수 선언과 동시에 메모리 할당, 원소들은 제로값으로 초기화.
+	// s := []int{0, 0, 0, 0, 0} // 변수 선언과 동시에 메모리 할당, 원소 초기화
 
-	for _, value := range s {
-		fmt.Println(value)
-	}
+	// s[4] = 100
+	// s[0] = 7
+	// s[1] = 91
 
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
+	// for _, value := range s {
+	// 	fmt.Println(value)
+	// }
 
-	test := [3]string{"인하", "go", "학생"}
-	testS := test[0:2]
-	testS2 := test[1:]
+	// copyS := s[1:4]
+	// for _, value := range copyS {
+	// 	fmt.Println(value)
+	// }
 
-	// testS[1] = "python"
-	// testS2[0] = "python"
-	// go는 역순 인덱싱 안 됨. -1, -2...
+	// test := [3]string{"인하", "go", "학생"}
+	// testS := test[0:2]
+	// testS2 := test[1:]
 
-	fmt.Println(test, len(test))
-	fmt.Println(testS, len(testS))
-	fmt.Println(testS2, len(testS2))
+	// // testS[1] = "python"
+	// // testS2[0] = "python"
+	// // go는 역순 인덱싱 안 됨. -1, -2...
+
+	// fmt.Println(test, len(test))
+	// fmt.Println(testS, len(testS))
+	// fmt.Println(testS2, len(testS2))
 }
